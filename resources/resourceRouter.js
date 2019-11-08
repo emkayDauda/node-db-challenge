34946dd85af41ddee12dc429d27ffc0b3b27ad6e
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-08 11:17:48 +0100

    :sparkles: Add validator middleware for tasks and resources

--- resources/resourceRouter.js
+++ resources/resourceRouter.js
@@ -10,16 +10,40 @@ resources.get("/", (req, res) => {
     .catch(err => res.status(500).json({ message: err.message }));
 });
 
-resources.get("/:id", (req, res) => {
-  db.getResources(req.params.id)
-    .then(resources => res.status(200).json(resources))
-    .catch(err => res.status(500).json({ message: err.message }));
+resources.get("/:id", resourceIdValidator, (req, res) => {
+  res
+    .status(200)
+    .json({ error: false, message: "Success", data: req.valResource });
 });
 
-resources.post("/", (req, res) => {
-  db.insert(req.body)
+resources.post("/", resourceBodyValidator, (req, res) => {
+  db.insert(req.valResBody)
     .then(resource => res.status(200).json(resource))
     .catch(err => res.status(500).json({ message: err.message }));
 });
+
+function resourceIdValidator(req, res, next) {
+  db.getResources(req.params.id)
+    .then(resource => {
+      if (resource) {
+        req.valResource = resource;
+        next();
+      }
+    })
+    .catch(err => res.status(500).json({ message: err.message }));
+}
+
+function resourceBodyValidator(req, res, next){
+  const { resource_name, description } = req.body
+  if(!Object.keys(req.body).length){
+      res.status(400).json({error: true, message: 'Request body missing'})
+  } else if (!resource_name ) {
+    res.status(400).json({error: true, message: 'Required params missing'})
+  }
+  else {
+      req.valResBody = { resource_name, description }
+      next()
+  }
+}
 
 module.exports = resources;
--- tasks/tasksRouter.js
+++ tasks/tasksRouter.js
@@ -19,7 +19,7 @@ tasks.get("/", (req, res) => {
     .catch(err => res.status(500).json({ message: err.message }));
 });
 
-tasks.get("/:id", (req, res) => {
+tasks.get("/:id", taskIdValidator, (req, res) => {
   db.get(req.params.id)
     .then(task => res.status(200).json({
         ...task,
@@ -28,10 +28,35 @@ tasks.get("/:id", (req, res) => {
     .catch(err => res.status(500).json({ message: err.message }));
 });
 
-tasks.post("/", (req, res) => {
-  db.insert(req.body)
+tasks.post("/", taskBodyValidator, (req, res) => {
+  db.insert(req.valTaskBody)
     .then(data => res.status(201).json(data))
     .catch(err => res.status(500).json({ message: err.message }));
 });
+
+function taskIdValidator(req, res, next) {
+  db.get(req.params.id)
+    .then(task => {
+      if (task) {
+        req.valTask = task;
+        next();
+      }
+    })
+    .catch(err => res.status(500).json({ message: err.message }));
+}
+
+function taskBodyValidator(req, res, next){
+  const { project_id, description, notes, completed } = req.body
+  if(!Object.keys(req.body).length){
+      res.status(400).json({error: true, message: 'Request body missing'})
+  } else if (!description || !project_id ) {
+    res.status(400).json({error: true, message: 'Required params missing'})
+  }
+  else {
+      req.valTaskBody = { project_id, description, notes }
+      if(completed) req.valTaskBody.completed = completed
+      next()
+  }
+}
 
 module.exports = tasks;
