b08e7eef4ac8ac2a718e294dccf39c12b362a357
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-08 09:51:11 +0100

    :sparkles: use projectsRouter in server.js

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const cors = require('cors')
 
 const server = express()
 
+const projectsRouter = require('./projects/projectsRouter')
+
 server.use(express.json())
 server.use(cors())
 server.use(helmet())
+
+server.use('/api/projects', projectsRouter)
 
 server.get('/', (req, res) => {
     res.send("This works")
