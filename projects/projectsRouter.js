03af7a6b8941b988abbab3b3e448d81748bc33e7
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-08 11:01:34 +0100

    :sparkles: Create validators for projectRouter

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -17,26 +17,44 @@ projects.get('/', (req, res) => {
     .catch(err => res.status(500).json({message: err.message}))
 })
 
-projects.get('/:id', (req, res) => {
-    dbHelper.getProjects(req.params.id)
-    .then(project => {
-        res.status(200).json({...project, completed: project.completed ? true : false})
-    })
-    .catch(err => res.status(500).json({message: err.message}))
+projects.get('/:id', projectIdValidator, (req, res) => {
+    res.status(200).json(req.valProject)
+   
 })
 
-projects.get('/:id/resources', (req, res) => {
-    resource.getProjectResources(req.params.id)
+projects.get('/:id/resources', projectIdValidator, (req, res) => {
+    resource.getProjectResources(req.valProject.project_id)
     .then(resources => {
         res.status(200).json(resources)
     })
     .catch(err => res.status(500).json({message: err.message}))
 })
 
-projects.post('/', (req, res) => {
-    dbHelper.add(req.body)
+projects.post('/', projectBodyValidator, (req, res) => {
+    dbHelper.add(req.valProjBody)
     .then(data => res.status(200).json(data))
     .catch(err => res.status(500).json({message: err.message}))
 })
+
+function projectIdValidator(req, res, next) {
+    const id = req.params.id
+    dbHelper.getProjects(id)
+    .then(project => {
+        req.valProject = {...project, completed: project.completed ? true : false}
+        next()
+    })
+    .catch(err => res.status(500).json({message: err.message}))
+}
+
+function projectBodyValidator(req, res, next){
+    const { project_name } = req.body
+    if(!Object.keys(req.body).length || !project_name){
+        res.status(400).json({error: true, message: 'Required params missing'})
+    }
+    else {
+        req.valProjBody = { project_name }
+        next()
+    }
+}
 
 module.exports = projects;
