a1596676d4bfd7cd5a5aa75cbb6d641ea29b6755
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-08 09:15:37 +0100

    :sparkles: Create base server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+server.use(helmet())
+
+server.get('/', (req, res) => {
+    res.send("This works")
+})
+
+module.exports = server;
