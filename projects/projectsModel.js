f7326ffbc18db5c542928f1b8f24a01ea20201ca
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-08 09:46:28 +0100

    :sparkles: Create projectsModel for db access to projects table

---
+++ projects/projectsModel.js
@@ -0,0 +1,22 @@
+const db = require('../data/dbConfig')
+
+function getProjects(id) {
+    let query = db('projects as p')
+
+    if (id) {
+        query.where('p.project_id', id)
+    }
+
+    return query;
+}
+
+function add(project) {
+    return db('projects')
+    .insert(project)
+    .then(([id]) => id ? this.getProjects(id) : null)
+}
+
+module.exports = {
+    getProjects,
+    add,
+}
